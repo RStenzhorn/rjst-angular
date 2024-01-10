06a4575ce23fc739d142abd8fa2ee87a3b5bec58
RStenzhorn <robert.stenzhorn@outlook.de>
2024-01-11 00:33:44 +0100

    sql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import { SqlAreaComponent } from './component/sql-area/sql-area.component';
 import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SqlAreaComponent
   ],
   imports: [
     BrowserModule,
